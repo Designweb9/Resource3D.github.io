c746985f37f6892bbf9c8b79bd2e98fc860b9610
Designerrweb <dchar982@gmail.com>
2022-04-04 11:47:22 -0700

    import
     file

---
+++ javascript.js
@@ -0,0 +1,14 @@
+var arrVd=["02.mp4","teclado.mp4","01.mp4"];
+
+const show=()=>{
+    var vd=document.getElementById("vdoRand")
+    var hm5=document.getElementById("htmlid")
+    var randNum=Math.floor(Math.random()*3);
+    vd.setAttribute("src","vdeo/"+arrVd[randNum]);
+    hm5.addEventListener("click",()=>{
+        vd.play();
+    })
+
+}
+
+window.addEventListener("load",show)
